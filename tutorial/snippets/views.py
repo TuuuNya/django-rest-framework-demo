630daedc1a92eb1358e2bbb90a473e7dfc552c4e
王松_Striker <song@secbox.cn>
2018-05-10 15:48:39 +0800

    :sparkles: use generic CBV `http://www.django-rest-framework.org/tutorial/3-class-based-views/#using-generic-class-based-views`

--- tutorial/snippets/views.py
+++ tutorial/snippets/views.py
@@ -1,36 +1,16 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from rest_framework import status
-from rest_framework.decorators import APIView
-from rest_framework.response import Response
 from tutorial.snippets.models import Snippet
 from tutorial.snippets.serializers import SnippetSerializer
-from django.http import Http404
-from rest_framework import mixins
 from rest_framework import generics
 
 
-class SnippetList(mixins.ListModelMixin, mixins.CreateModelMixin, generics.GenericAPIView):
+class SnippetList(generics.ListCreateAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
 
-    def get(self, request, *args, **kwargs):
-        return self.list(request, *args, **kwargs)
 
-    def post(self, request, *args, **kwargs):
-        return self.create(request, *args, **kwargs)
-
-
-class SnippetDetail(mixins.RetrieveModelMixin, mixins.UpdateModelMixin, mixins.DestroyModelMixin, generics.GenericAPIView):
+class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
-
-    def get(self, request, *args, **kwargs):
-        return self.retrieve(request, *args, **kwargs)
-
-    def put(self, request, *args, **kwargs):
-        return self.update(request, *args, **kwargs)
-
-    def delete(self, request, *args, **kwargs):
-        return self.destroy(request, *args, **kwargs)
