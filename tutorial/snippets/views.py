8f0809c83a4a187c9bc551ed1e853422d240b12b
王松_Striker <song@secbox.cn>
2018-05-10 19:12:59 +0800

    :fire: delete useless code,  Tutorial 6 final version.

--- tutorial/snippets/views.py
+++ tutorial/snippets/views.py
@@ -3,13 +3,11 @@ from __future__ import unicode_literals
 
 from tutorial.snippets.models import Snippet
 from tutorial.snippets.serializers import SnippetSerializer, UserSerializer
-from rest_framework import generics
 from django.contrib.auth.models import User
 from rest_framework import permissions
 from tutorial.snippets.permissions import IsOwnerOrReadOnly
-from rest_framework.decorators import api_view, action
+from rest_framework.decorators import action
 from rest_framework.response import Response
-from rest_framework.reverse import reverse
 from rest_framework import renderers
 from rest_framework import viewsets
 
--- tutorial/urls.py
+++ tutorial/urls.py
@@ -22,7 +22,6 @@ router.register(r'users', views.UserViewSet)
 router.register(r'groups', views.GroupViewSet)
 
 urlpatterns = [
-    # url(r'^', include(router.urls)),
     url(r'^', include('tutorial.snippets.urls')),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
