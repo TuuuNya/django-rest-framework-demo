ae50deb371d31593e01f10648b2261da59b8b9ea
王松_Striker <song@secbox.cn>
2018-05-10 16:44:08 +0800

    :sparkles:  add user permission and login `http://www.django-rest-framework.org/tutorial/4-authentication-and-permissions/#adding-login-to-the-browsable-api`

--- tutorial/snippets/views.py
+++ tutorial/snippets/views.py
@@ -5,16 +5,22 @@ from tutorial.snippets.models import Snippet
 from tutorial.snippets.serializers import SnippetSerializer, UserSerializer
 from rest_framework import generics
 from django.contrib.auth.models import User
+from rest_framework import permissions
 
 
 class SnippetList(generics.ListCreateAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
+    permissions = (permissions.IsAuthenticatedOrReadOnly, )
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
 
 
 class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
+    permissions = (permissions.IsAuthenticatedOrReadOnly, )
 
 
 class UserList(generics.ListAPIView):
